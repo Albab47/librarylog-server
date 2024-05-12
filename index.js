1f8b7eee2c6ad78d83c9cf490533b8bcc7beb99f
Albab47 <115328782+Albab47@users.noreply.github.com>
2024-05-12 14:13:30 +0600

    Setup project

---
+++ index.js
@@ -0,0 +1,59 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+require("dotenv").config();
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.173efa4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+const corsOptions = {
+  origin: ["http://localhost:5000"],
+  credentials: true,
+};
+
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+
+// Middlewares
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
+// Create a MongoClient
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// Testing
+app.get("/", (req, res) => {
+  res.send("libraryLog server is running");
+});
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
