3b386783557639ca722ea8c2ee1f6be12ef2fc27
Albab47 <115328782+Albab47@users.noreply.github.com>
2024-05-14 18:34:43 +0600

    Add api to check if book exist in borrowed books

--- index.js
+++ index.js
@@ -34,6 +34,38 @@ const client = new MongoClient(uri, {
   },
 });
 
+// function to generate token
+const generateToken = (payload) => {
+  const secretKey = process.env.ACCESS_TOKEN_SECRET;
+  const options = {
+    expiresIn: "1h",
+  };
+  const token = jwt.sign(payload, secretKey, options);
+  return token;
+};
+
+// function to verify token
+const verifyToken = (req, res, next) => {
+  const secretKey = process.env.ACCESS_TOKEN_SECRET;
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+
+  if (token) {
+    jwt.verify(token, secretKey, (err, decoded) => {
+      if (err) {
+        return res.status(401).send({ message: "unauthorized access" });
+      } else {
+        console.log(decoded);
+        req.user = decoded;
+        next();
+      }
+    });
+  }
+};
+
 async function run() {
   try {
     // await client.connect();
@@ -42,6 +74,27 @@ async function run() {
     const bookCollection = database.collection("books");
     const borrowedBookCollection = database.collection("borrowedBooks");
 
+    // -------------- Auth related API ------------------
+
+    // Generation token for user
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = generateToken(user);
+      res.cookie("token", token, cookieOptions).send({ success: true });
+    });
+
+    // Clear token from cookie
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      res
+        .clearCookie("token", { ...cookieOptions, maxAge: 0 })
+        .send({ success: true });
+    });
+
+
+
+    // ------------ Service related API ------------------
+
     // Get all categories
     app.get("/categories", async (req, res) => {
       const categories = await categoryCollection.find().toArray();
@@ -49,7 +102,11 @@ async function run() {
     });
 
     // Add new book data
-    app.post("/books", async (req, res) => {
+    app.post("/books", verifyToken, async (req, res) => {
+      const tokenEmail = req.user.email;
+      if(tokenEmail !== req.query.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
       const book = req.body;
       const result = await bookCollection.insertOne(book);
       res.send(result);
@@ -60,6 +117,9 @@ async function run() {
       let query = {};
       if (req.query.category) {
         query = { category: req.query.category };
+      }
+      if(req.query.filter) {
+        query = { quantity: {$gt: 0} }
       }
       const books = await bookCollection.find(query).toArray();
       res.send(books);
@@ -79,13 +139,18 @@ async function run() {
       res.send(book);
     });
 
-    // Get single book data by id
-    app.patch("/update-book/:id", async (req, res) => {
+    // Update book data by id
+    app.patch("/update-book/:id", verifyToken, async (req, res) => {
+      const tokenEmail = req.user.email;
+      console.log(tokenEmail, req.query.email);
+      if(tokenEmail !== req.query.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
       const updatedFields = req.body;
       const query = { _id: new ObjectId(req.params.id) };
       const updateDoc = {
-        $set: {...updatedFields}
-      }
+        $set: { ...updatedFields },
+      };
       const result = await bookCollection.updateOne(query, updateDoc);
       res.send(result);
     });
@@ -117,16 +182,26 @@ async function run() {
     });
 
     // Remove book data on return
-    app.delete("/borrowed-books/:id", async (req, res) => {    
-      const query = { _id: new ObjectId(req.params.id)};
+    app.delete("/borrowed-books/:id", async (req, res) => {
+      const query = { _id: new ObjectId(req.params.id) };
       console.log(query);
       const result = await borrowedBookCollection.deleteOne(query);
       console.log(result);
-      res.send(result)
+      res.send(result);
     });
 
-
-
+    // Check if a user's book exist in borrowed books
+    app.get("/borrowed-books/find/:id", async (req, res) => {
+      const email = req.query.email;
+      const id = req.params.id;
+      console.log(email, id);
+      const query = { bookId: id, 'borrower.email': email};
+      const options = {projection: { _id: 0, bookId: 1, name: 1 },}
+      const book = await borrowedBookCollection.findOne(query, options);
+      console.log(book);
+      res.send(book);
+    });
+    
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
